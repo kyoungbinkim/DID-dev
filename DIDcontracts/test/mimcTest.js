0989023edf25edb57df97562f68e637e69a6e15b
kim <kyoungbin1996@gmail.com>
2022-12-30 11:20:05 +0900

    rm test

--- DIDcontracts/test/mimcTest.js
+++
@@ -1,8 +0,0 @@
-const VerifySchnorr = artifacts.require("VerifySchnorr");
-
-contract('mimc7 test', ()=> {
-    it('hello', async () => {
-        const VerifySchnorr = await VerifySchnorr.deployed();
-        const mimc7result = await VerifySchnorr.hash(123,123);
-    })
-})
